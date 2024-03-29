fc974fe8d2a30bf28f8533f742837676cdd6e0e7
Soyon Lee <slee550@myseneca.ca>
2024-03-29 11:07:17 -0400

    initial commit

---
+++ reactproject1/src/components/Question.jsx
@@ -0,0 +1,26 @@
+import { useContext } from 'react'
+import { QuizContext } from '../contexts/quiz'
+import Answer from './Answer'
+
+const Question = ({ questions }) => {
+    const [quizState, dispatch] = useContext(QuizContext);
+    const currentQuestion = quizState.questions[quizState.currentQuestionIndex];
+    return (
+        <div>
+            <div className="question">{currentQuestion.question}</div>
+            <div className="answers">
+                {quizState.answers.map((answer, index) => (
+                    <Answer answerText={answer}
+                        key={index}
+                        index={index}
+                        currentAnswer={quizState.currentAnswer}
+                        correctAnswer={currentQuestion.correctAnswer}
+                        onSelectAnswer={(answerText) => dispatch({type: "SELECT_ANSWER", payload: answerText})}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Question
---
+++ reactproject1/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Quiz from './components/Quiz.jsx'
+import { QuizProvider } from './contexts/quiz'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <QuizProvider>
+        <Quiz />
+    </QuizProvider>,
+)
